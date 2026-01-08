cdfd517707c07303f44fae4071f232199251e8ef
Michael Bushyeager <165829618+mbushyeager05@users.noreply.github.com>
2026-01-08 00:41:24 +0000

    Add initial JavaScript alert for Week 0

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,9 @@
 
 console.log('Portfolio loaded');
 
+
+//Week 0: Hello World!
+alert('I\'m JavaScript!');
 // Week 1: You'll add theme toggle code here
 // Week 2: You'll add localStorage persistence here
 // Week 3+: More to come...
